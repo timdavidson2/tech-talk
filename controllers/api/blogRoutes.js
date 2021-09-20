fbc461fc6f770d5a94b6d3bcd2cf990cee09da3c
Tim Davidson <timdavidson2@gmail.com>
2021-09-20 12:17:31 -0400

    Update blogRoutes.js

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,8 +1,9 @@
-const router = require('express').Router();
-const { Blog } = require('../../models');
-const withAuth = require('../../utils/auth');
+// initialize modules
+const router = require("express").Router();
+const { Blog } = require("../../models");
+const withAuth = require("../../utils/auth");
 
-router.post('/', withAuth, async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   try {
     const newBlog = await Blog.create({
       ...req.body,
@@ -15,7 +16,7 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
+router.delete("/:id", withAuth, async (req, res) => {
   try {
     const blogData = await Blog.destroy({
       where: {
@@ -25,7 +26,7 @@ router.delete('/:id', withAuth, async (req, res) => {
     });
 
     if (!blogData) {
-      res.status(404).json({ message: '404 Blog ID not found' });
+      res.status(404).json({ message: "404 Blog ID not found" });
       return;
     }
 
