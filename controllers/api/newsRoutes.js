8a48ea439f224b8ff5ad78dee7c72fea8f4ea206
Tim Davidson <timdavidson2@gmail.com>
2021-09-21 17:31:46 -0400

    Update newsRoutes.js

--- controllers/api/newsRoutes.js
+++ controllers/api/newsRoutes.js
@@ -1,2 +1,19 @@
 const NewsAPI = require("newsapi");
 require("dotenv").config();
+const newsapi = new NewsAPI(APIKEY);
+
+newsapi.v2
+  .sources({
+    category: "technology",
+    language: "en",
+    country: "us",
+  })
+  .then((response) => {
+    console.log(response);
+    /*
+    {
+      status: "ok",
+      sources: [...]
+    }
+  */
+  });
